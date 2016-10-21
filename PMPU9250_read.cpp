7e7513cadfe896b81bb0d312eb31be604e53710b
Khurram <khurram@Ahsans-MacBook-Pro.local>
2016-10-21 18:07:00 +0300

    read who am i ak8963

--- PMPU9250_read.cpp
+++ PMPU9250_read.cpp
@@ -13,18 +13,19 @@ int main()
 	
 	mpu9250.initMPU9250();
 	delay(10);
-	mpu9250.reIntAK8963();
+	//mpu9250.reIntAK8963();
 	cout << "Who AM I> AK8963 ?: " <<  (int) mpu9250.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963) << endl;
-	
+	mpu9250.initAK8963(data);
+	cout << "Who AM I> AK8963 ?: " <<  (int) mpu9250.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963) << endl;
 	//cout << "WHO AM 2?: " <<   std::hex <<   (char) mpu9250.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250) << endl;
 	//cout << "WHO AM 2 Mag?: " <<  (char) mpu9250.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963) << endl;
 
-	delay(10);
-	mpu9250.writeByte(AK8963_ADDRESS, AK8963_CNTL, 0x01);
-	delay(10);
-	mpu9250.initAK8963(data2);
-	delay(10);
-	mpu9250.writeByte(AK8963_ADDRESS, AK8963_CNTL, 0x01);
+	//delay(10);
+	//mpu9250.writeByte(AK8963_ADDRESS, AK8963_CNTL, 0x01);
+	//delay(10);
+	//mpu9250.initAK8963(data2);
+	//delay(10);
+	//mpu9250.writeByte(AK8963_ADDRESS, AK8963_CNTL, 0x01);
 
 	//cout << "WHO AM 3?: " <<   std::hex <<   (char) mpu9250.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250) << endl;
 	//cout << "WHO AM 3 Mag?: " <<  (char) mpu9250.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963) << endl;
