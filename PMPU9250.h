503770eb5671382c7f4016406dc1dcba1c68eeb1
Khurram <khurram@Ahsans-MacBook-Pro.local>
2016-10-21 16:49:02 +0300

    read who am i ak8963

--- PMPU9250.h
+++ PMPU9250.h
@@ -283,16 +283,16 @@ public:
 	{
 		// Initialize MPU9250 device
 		// wake up device
-		cout << "Power Management before setting?: " <<  (int) readByte(MPU9250_ADDRESS, PWR_MGMT_1) << endl;
-	
+		
 		writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x00); // Clear sleep mode bit (6), enable all sensors 
-		//delay(10); // Delay 100 ms for PLL to get established on x-axis gyro; should check for PLL ready interrupt  
-
-		cout << "Power Management after setting?: " <<  (int) readByte(MPU9250_ADDRESS, PWR_MGMT_1) << endl;
-	
+		delay(100); // Delay 100 ms for PLL to get established on x-axis gyro; should check for PLL ready interrupt  
+		
+		cout << "Power Management before setting?: " <<  (int) readByte(MPU9250_ADDRESS, PWR_MGMT_1) << endl;
 		// get stable time source
 		writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x01);  // Set clock source to be PLL with x-axis gyroscope reference, bits 2:0 = 001
-
+		cout << "Power Management after setting?: " <<  (int) readByte(MPU9250_ADDRESS, PWR_MGMT_1) << endl;
+		
+		
 		// Configure Gyro and Accelerometer
 		// Disable FSYNC and set accelerometer and gyro bandwidth to 44 and 42 Hz, respectively; 
 		// DLPF_CFG = bits 2:0 = 010; this sets the sample rate at 1 kHz for both
