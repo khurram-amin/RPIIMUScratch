3a890583146263fe172880bc38774e805c3d611a
Khurram Amin <khurram@Khurrams-MacBook-Pro.local>
2016-11-08 20:25:50 +0300

    Some minor bug fixes

--- PMPU9250_read.cpp
+++ PMPU9250_read.cpp
@@ -13,9 +13,9 @@ int main()
 	
 	mpu9250.initMPU9250();
 	delay(10);
-	mpu9250.reIntAK8963();
+	//mpu9250.reIntAK8963();
 	cout << "Who AM I> AK8963 ?: " <<  (int) mpu9250.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963) << endl;
-	mpu9250.initAK8963(data2);
+	//mpu9250.initAK8963(data2);
 	cout << "Who AM I> AK8963 ?: " <<  (int) mpu9250.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963) << endl;
 	//cout << "WHO AM 2?: " <<   std::hex <<   (char) mpu9250.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250) << endl;
 	//cout << "WHO AM 2 Mag?: " <<  (char) mpu9250.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963) << endl;
